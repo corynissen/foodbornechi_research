73495820722b43aedaa7c862b0dfc896c02ca837
corynissen <corynissen@gmail.com>
2013-12-02 19:07:05 -0600

    cleaned up code, changed colors on maps, created restaurant count by ward and restaurant count gradient maps saved to output folder

--- map_chicago_restaurants.R
+++ map_chicago_restaurants.R
@@ -17,45 +17,35 @@ rest.summary <- ddply(rest.df, .(ward), summarize, count=length(ward))
 
 # read the shapefile
 shp <- readOGR("wards", "Wards")
-shp <- spTransform(shapefile, CRS("+proj=longlat +datum=WGS84"))
-
+shp <- spTransform(shp, CRS("+proj=longlat +datum=WGS84"))
 # assign an ID to each polygon, and pull out the individual points
 shp@data$id <- rownames(shp@data)
 pts <- fortify(shp, region="id")
-
 # merge in the other shapefile data
 shp.df <- merge(pts, shp,by="id")
-
-# merge in the crime data
+# merge in the restaurant data
 shp.df <- merge(shp.df, rest.summary, by.x="WARD", by.y="ward")
-
 # shp.df is all kinds of out of order right now. need to order by WARD,order
 # or we end up with a broken polygon path
 shp.df <- shp.df[order(shp.df$id, shp.df$order),]
 
-# now for the graph
-ggplot(shp.df,aes(long,lat,group=group)) + geom_polygon(aes(fill=count))
-
-# same graph using ggmap package with google maps
-p1 <- qmap("chicago", darken=.1) 
-p2 <- p1 +geom_polygon(data=shp.df, aes(x=long, y=lat, group=group, fill=count), 
-               color="white", size=.1) +
+# graph using ggmap package with cloudmade maps
+cloudmadekey <- scan("~/cn/personal/keys/cloudmadekey.txt", what="character")
+p1 <- qmap("chicago", source="cloudmade", api_key=cloudmadekey, maptype=108995)
+p2 <- p1 +geom_polygon(data=shp.df, 
+  aes(x=long, y=lat, group=group, fill=count), color="white", size=.15) + 
+  theme(legend.position=c(.9, .83)) + 
+  scale_fill_gradient(name="Restaurant\nCount", low="#001F4C", high="#E6F0FF") +
   coord_cartesian(xlim=c(-87.96, -87.5), ylim=c(41.62, 42.05))
-ggsave(plot=p2, "output/chi_restaurants.png", height=5, width=5)
+ggsave(plot=p2, "output/chi_restaurant_count_by_ward.png", height=5, width=5)
 
-p2 <- p1 + stat_density2d(data=rest.df, aes(x=lng.combined, y=lat.combined, 
-      fill=..level..), geom="polygon") +
+p3 <- p1 + stat_density2d(data=rest.df,   
+  aes(x=lng.combined, y=lat.combined, fill=..density..), geom="tile", 
+    contour=F) +
   scale_fill_gradient(low="gray1", high="gray99") +
+  theme(legend.position=c(.9, .83)) + 
   geom_polygon(data=shp.df, aes(x=long, y=lat, group=group), 
-               fill="grey", color="white", size=.1, alpha=.2) +
+    fill="grey", color="white", size=.15, alpha=.02) +
   coord_cartesian(xlim=c(-87.96, -87.5), ylim=c(41.62, 42.05))
-ggsave(plot=p2, "output/chi_restaurants.png", height=5, width=5)
-
-p2 <- p1 + stat_density2d(data=rest.df, aes(x=lng.combined, y=lat.combined, 
-                                            fill=..density..), geom="tile", contour=F) +
-  scale_fill_gradient(low="gray1", high="gray99") +
-  geom_polygon(data=shp.df, aes(x=long, y=lat, group=group), 
-               fill="grey", color="white", size=.1, alpha=.02) +
-  coord_cartesian(xlim=c(-87.96, -87.5), ylim=c(41.62, 42.05))
-ggsave(plot=p2, "output/chi_restaurants.png", height=5, width=5)
+ggsave(plot=p3, "output/chi_restaurant_count_gradient.png", height=5, width=5)
 
